f1d01994691eda0ea5cc9490a12026b7057a2af5
YourGitHubUsername <your_email@example.com>
2025-04-12 01:22:24 +0100

    updated styles

--- src/data/claimsData.js
+++ src/data/claimsData.js
@@ -3,19 +3,19 @@ const claims = [
     {
       id: 1,
       date: "April 11th 2025",
-      text: "Nigeria to adopt cryptocurrency as national currency by June 2025...",
+      text: " Nigeria to adopt cryptocurrency as national currency by June 2025...",
       image: "/assets/NigCrypto1.jpg"
     },
     {
       id: 2,
       date: "April 10th 2025",
-      text: "President signs new bill allowing free internet access nationwide...",
+      text: " President signs new bill allowing free internet access nationwide...",
       image: "/assets/NigBill1.jpg"
     },
     {
       id: 3,
       date: "April 9th 2025",
-      text: "Drinking hot water every morning cures all viral infections...",
+      text: " Drinking hot water every morning cures all viral infections...",
       image: "/assets/hotWater.jpg"
     }
   ];
