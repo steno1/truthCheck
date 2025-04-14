186d5f36adadd20b9aced3c120ad276c9bab8e28
YourGitHubUsername <your_email@example.com>
2025-04-14 17:33:29 +0100

    Update to use backend base URL from deployed server

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -1,13 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-
-const baseUrl = process.env.REACT_APP_GOOGLE_FACT_CHECK_BASE_URL;
+const baseUrl = process.env.REACT_APP_BACKEND_BASE_URL;
 
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl }),  
-
+  reducerPath: 'api', 
+  baseQuery: fetchBaseQuery({ baseUrl }),
   tagTypes: ['Check', 'Feedback', 'Report'],
   endpoints: (builder) => ({}),
 });
 
-export default apiSlice.reducer;
+export default apiSlice;
