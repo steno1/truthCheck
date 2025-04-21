af8638a64dded35651de648c732e4489b4c602a5
YourGitHubUsername <your_email@example.com>
2025-04-21 07:47:00 +0100

    update

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -1,19 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-
-const baseUrl = process.env.REACT_APP_BACKEND_BASE_URL; 
-
-
+const baseUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_BACKEND_BASE_URL
+    : 'http://localhost:5000';
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl }),
-
   tagTypes: ['Check', 'Feedback', 'Report'],
   endpoints: (builder) => ({}),
 });
 
 export default apiSlice.reducer;
-
-
-
-
